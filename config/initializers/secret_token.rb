ee2009a0984c593939762b487c83a345be28afa1
Katie Miller <phedinkus@gmail.com>
2010-11-16 19:13:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findmeat::Application.config.secret_token = 'a02c548e6c3b0ad59a37afd75f50cf0237b371fb4f56826e5ccb38e78eb093cae7e5770d74ea7df24b275e3e88a67269b57b3a7037d65616f167cfa8bf523751'
