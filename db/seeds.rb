efa82a88d41ff9e955fc235c79d2c9f910976a5b
Katie Miller <phedinkus@gmail.com>
2010-11-22 21:30:54 -0800

    Added content to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Meat.delete_all
+
+Meat.create(:animal => 'chicken', :kind => 'drumstick')
+Meat.create(:animal => 'cow', :kind => 'ribeye steak')
+Meat.create(:animal => 'cow', :kind => 'ground beef')
+Meat.create(:animal => 'deer', :kind => 'venison')
+Meat.create(:animal => 'pig', :kind => 'bacon')
+Meat.create(:animal => 'pig', :kind => 'pork belly')
+Meat.create(:animal => 'horse', :kind => 'steak')
+
